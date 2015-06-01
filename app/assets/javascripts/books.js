fa70f36bc41b5182f7ccd2c0e21e777d3fd7fb0e
shane <sssims@ucsc.edu>
2015-05-31 17:33:03 -0700

    Bug Fix: Post Modal wouldn't generate sometimes.

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,3 +1,37 @@
+function load_post_modal() {
+
+  $.ajax({   
+    url: "/books/index",
+    success: function (result) {
+      $("#books_ajax_target").html(result);
+    }
+  });
+
+}
+
+$(document).ready(function() {
+  load_post_modal();
+});
+
+$(document).on("page:load", function() {
+  load_post_modal();
+});
+
+$(document).keyup(function (e) {
+  if(e.keyCode == 27) {
+    $("#overlay").css("visibility", "hidden");
+    //$("body").removeClass("no-overflow");
+  }
+});
+
+$(document).on("click", "#post_book_link", function() {
+  el = document.getElementById("overlay");
+  el.style.visibility = "visible";
+  //$("body").addClass("no-overflow");
+  //$("body").addClass("no-overflow");
+  $("#book_search_field").focus();
+});
+
 /*
 $(document).on("click", ".info-switch", function() {
      $(this).parent().parent().children("#wrapper > #book-info").removeClass("hidden");
